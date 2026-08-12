ba1e3f66c0d862d0fb0e22c4250c5fca2fb7435b
fnzh-i <fnzh.4u@gmail.com>
2026-08-13 02:28:48 +0800

    Hotfix import components
    + update minor issue "next" to "react"

--- Frontend/src/app/App.tsx
+++ Frontend/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router";
 import { router } from "./Route";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
