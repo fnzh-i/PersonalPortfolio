3bae9b48605fe43913d1bf045b72d59671bc17e9
fnzh-i <fnzh.4u@gmail.com>
2026-08-13 01:55:48 +0800

    added components for web analytics
    + added analytics and speed insights

--- Frontend/src/app/App.tsx
+++ Frontend/src/app/App.tsx
@@ -1,8 +1,16 @@
 import { RouterProvider } from "react-router";
 import { router } from "./Route";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-    return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
 }
 
-export default App
+export default App;
